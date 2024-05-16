68b6f05ac5e66c77733c7df3c1c3a42fa6be8c91
Samuel Sotelo <prueba@correo.com>
2024-05-16 11:57:33 -0500

    json

--- js/userForm.js
+++ js/userForm.js
@@ -6,6 +6,104 @@ function mostrarImagen(input) {
             document.querySelector('.userImage').style.backgroundImage = `url('${e.target.result}')`;
         }
 
-        reader.readAsDataURL(input.files[0]); // convert to base64 string
+        reader.readAsDataURL(input.files[0]);
     }
+}
+
+// Funciones para almacenar en el JSON.
+// URL
+const URL = "http://localhost:3000/userRegistration";
+
+// Llamar los input a objetos
+const userForm = document.querySelector('#userForm');
+const idUser = Date.now().toString(30);
+const imageUser = document.querySelector('#fileInput');
+const namesInput = document.querySelector('#namesInput');
+const lastNamesInput = document.querySelector('#lastNamesInput');
+const documentType = document.querySelector('#documentType');
+const documentNumberInput = document.querySelector('#documentNumberInput');
+const phoneInput = document.querySelector('#phoneInput');
+const smartPhoneInput = document.querySelector('#smartPhoneInput');
+const emailInput = document.querySelector('#emailInput');
+const passwordInput = document.querySelector('#passwordInput');
+const passwordInput2 = document.querySelector('#passwordInput2');
+const addressInput = document.querySelector('#addressInput');
+
+document.addEventListener("DOMContentLoaded", () => {
+    //Llamar la función encargada de obtener los usuarios
+
+    userForm.addEventListener("submit", (event) => {
+        //Quitar acciones por defecto
+        event.preventDefault();
+        //Llamamos la función que se encarga de agregar al usuario
+        verifyingInformation();
+    });
+});
+
+async function verifyingInformation(){
+    if (passwordInput.value === passwordInput2.value){
+        await addUser();
+    }
+    else{
+        alert('Las contraseñas no coinciden.');
+    }
+}
+
+async function addUser() {
+    const newUser = {
+        image: imageUser.value,
+        names: namesInput.value,
+        lastNames: lastNamesInput.value,
+        documentType: documentType.value,
+        documentNumber: documentNumberInput.value,
+        phone: phoneInput.value,
+        smartPhone: smartPhoneInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+        address: addressInput.value
+    };
+    
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        });
+
+        if (response.ok) {
+            console.log('Usuario añadido correctamente.');
+            clearForm();
+            redirect();
+        } else {
+            throw new Error('Error adding user');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        Swal.fire({
+            title: 'Error!',
+            text: 'Ah ocurrido un error al guardar el usuario.',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        });
+    }
+}
+
+function clearForm() {
+    imageUser.value = "";
+    namesInput.value = "";
+    lastNamesInput.value = "";
+    documentType.value = "";
+    documentNumberInput.value = "";
+    phoneInput.value = "";
+    smartPhoneInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
+    passwordInput2.value = "";
+    addressInput.value = "";
+}
+
+function redirect(){
+    window.location.replace("../index.html");
 }
