d29e1131b7831c0bc5a5ce726c4c1a95825869bf
Samuel Sotelo <prueba@correo.com>
2024-05-16 06:08:56 -0500

    userFormResponsive

---
+++ js/userForm.js
@@ -0,0 +1,11 @@
+function mostrarImagen(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.querySelector('.userImage').style.backgroundImage = `url('${e.target.result}')`;
+        }
+
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
