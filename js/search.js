f2e74d9d9b2eb2eb583c4dd9e6eac9c91c217a39
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-05-22 10:51:06 -0500

    commit

---
+++ js/search.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const companyContainerSearch = document.querySelector("#companySearchContainer");
+    const cityFilter = localStorage.getItem('cityFilter');
+    const specialFilter = localStorage.getItem('specialFilter');
+
+})
